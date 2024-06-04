fc9959dd3d5f49974f055134e6d76feb73653b91
Greed <suby0475@gmail.com>
2024-06-04 16:24:50 +0900

    프로젝트 생성

---
+++ DdokTalk/DdokTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DdokTalkApp.swift
+//  DdokTalk
+//
+//  Created by Greed on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DdokTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
