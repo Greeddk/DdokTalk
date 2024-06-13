1fb1b2a3cfbb73b17e7d933083c5de798d117a60
Greed <suby0475@gmail.com>
2024-06-13 20:55:41 +0900

    chore(utility): hideKeyboard에서 deprecated된 코드 수정

--- DdokTalk/Utility/HideKeyboard.swift
+++ DdokTalk/Utility/HideKeyboard.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 extension UIApplication {
     func hideKeyboard() {
-        guard let window = windows.first else { return }
+        guard let windowScene = connectedScenes.first as? UIWindowScene, let window = windowScene.windows.first else { return }
         let tapRecognizer = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
         tapRecognizer.cancelsTouchesInView = false
         tapRecognizer.delegate = self
