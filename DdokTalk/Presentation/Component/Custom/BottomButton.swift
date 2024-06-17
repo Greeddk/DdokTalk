aba17a722ffbf84f6d8c025ef2e7b0243813a6da
Greed <suby0475@gmail.com>
2024-06-17 19:25:41 +0900

    feat(bottomButton): bool 값에 따라 버튼 활성화 기능 추가

--- DdokTalk/Presentation/Component/Custom/BottomButton.swift
+++ DdokTalk/Presentation/Component/Custom/BottomButton.swift
@@ -9,20 +9,20 @@ import SwiftUI
 
 struct BottomButton: View {
     var title: String
-    var backgroundColor: Color = Color.BackgroundPrimary
-    var buttonColor: Color
+    var isActive: Bool
     var action: () -> Void
     
     var body: some View {
         ZStack {
-            backgroundColor
+            Color.BackgroundPrimary
             VStack(spacing: 0) {
                 Button(action: action) {
                     HStack(spacing: 8) {
                         Text(title)
                     }
                 }
-                .buttonStyle(DTButtonStyle(size: CGSize(width: 323, height: 44), backgroundColor: buttonColor, fontColor: .white))
+                .disabled(!isActive)
+                .buttonStyle(DTButtonStyle(size: CGSize(width: 323, height: 44), backgroundColor: isActive ? Color.Point : Color.Inactive, fontColor: .white))
             }
             .padding(.vertical, 12)
         }
@@ -30,10 +30,5 @@ struct BottomButton: View {
         .frame(height: 68)
         .ignoresSafeArea(.keyboard)
     }
-    
-    func setBackgroundColor(_ color: Color) -> some View {
-        var view = self
-        view.backgroundColor = color
-        return view
-    }
+
 }
